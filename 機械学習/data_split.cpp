8e3f3e663a0f005f5e034bafefc0d7a9cf731874
komoriTT <komori1te14601y@gmail.com>
2019-08-09 15:44:53 +0900

    プログラムの変更

--- 機械学習/data_split.cpp
+++ 機械学習/data_split.cpp
@@ -8,13 +8,14 @@ int main(){
 
   std::ifstream input1;
   input1.open("data_file.txt");
-
+  std::ofstream output_all;
+  output_all.open("output.csv");
   std::ofstream output1;
-  output1.open("output1.txt");
+  output1.open("output1.csv");
   std::ofstream output2;
-  output2.open("output2.txt");
+  output2.open("output2.csv");
   std::ofstream output3;
-  output3.open("output3.txt");
+  output3.open("output3.csv");
 std::ofstream output4;
  output4.open("output4.txt");
   std::ofstream output5;
@@ -30,64 +31,76 @@ std::ofstream output4;
   std::ofstream output10;
   output10.open("output10.txt");
   
+output_all<<"pla1"<<","<<"pla2"<<","<<"pla3"<<","<<"pla4"<<","<<"pla5"<<","<<"pla6"<<","<<"pla7"<<","<<"pla8"<<","<<"pla9"<<","<<"pla10"<<","<<"iron1"<<","<<"iron2"<<","<<"iron3"<<","<<"iron4"<<","<<"iron5"<<","<<"iron6"<<","<<"iron7"<<","<<"iron8"<<","<<"iron9"<<","<<"incident"<<endl;
+output1<<"pla1"<<","<<"pla2"<<","<<"pla3"<<","<<"pla4"<<","<<"pla5"<<","<<"pla6"<<","<<"pla7"<<","<<"pla8"<<","<<"pla9"<<","<<"pla10"<<","<<"iron1"<<","<<"iron2"<<","<<"iron3"<<","<<"iron4"<<","<<"iron5"<<","<<"iron6"<<","<<"iron7"<<","<<"iron8"<<","<<"iron9"<<","<<"incident"<<endl;
 
   int counter=0;
   while(!input1.eof())
     {
-      double incident,p1,p2,p3,p4,i1,i2,i3,i4;
-      input1>>incident>>p1>>i1>>p2>>i2>>p3;
+      double incident,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10;
+      input1>>incident>>p1>>i1>>p2>>i2>>p3>>i3>>p4>>i4>>p5>>i5>>p6>>i6>>p7>>i7>>p8>>i8>>p9>>i9>>p10;
       counter=counter+1;
-      
+    output_all<<p1<<","<<p2<<","<<p3<<","<<p4<<","<<p5<<","<<p6<<","<<p7<<","<<p8<<","<<p9<<","<<p10<<","<<i1<<","<<i2<<","<<i3<<","<<i4<<","<<i5<<","<<i6<<","<<i7<<","<<i8<<","<<i9<<","<<incident<<endl;   
       if(counter<1000001)
 	{
 	  // output1<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output1<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
-	  
+	  output1<<p1<<","<<p2<<","<<p3<<","<<p4<<","<<p5<<","<<p6<<","<<p7<<","<<p8<<","<<p9<<","<<p10<<","<<i1<<","<<i2<<","<<i3<<","<<i4<<","<<i5<<","<<i6<<","<<i7<<","<<i8<<","<<i9<<","<<incident<<endl;
+	 //  output1<<p1<<p2<<p3<<p4<<p5<<p6<<p7<<p8<<p9<<p10<<i1<<i2<<i3<<i4<<i5<<i6<<i7<<i8<<i9<<incident<<endl;
 	}
       else if(counter<2000001)
 	{
 	  // output2<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output2<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	 // output2<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	output2<<p1<<","<<p2<<","<<p3<<","<<p4<<","<<p5<<","<<p6<<","<<p7<<","<<p8<<","<<p9<<","<<p10<<","<<i1<<","<<i2<<","<<i3<<","<<i4<<","<<i5<<","<<i6<<","<<i7<<","<<i8<<","<<i9<<","<<incident<<endl;
+	  
 	}
       else if(counter<3000001)
 	{
 	  // output3<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output3<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	 // output3<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  output3<<p1<<","<<p2<<","<<p3<<","<<p4<<","<<p5<<","<<p6<<","<<p7<<","<<p8<<","<<p9<<","<<p10<<","<<i1<<","<<i2<<","<<i3<<","<<i4<<","<<i5<<","<<i6<<","<<i7<<","<<i8<<","<<i9<<","<<incident<<endl;
+	
 	}
       else if(counter<4000001)
 	{
 	  //  output4<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output4<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	  output4<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  
 	}
       else if(counter<5000001)
 	{
 	  // output5<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output5<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
-	}
+	  output5<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  	}
       else if(counter<6000001)
 	{
 	  // output6<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output6<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	  output6<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  
 	}
       else if(counter<7000001)
 	{
 	  // output7<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output7<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	  output7<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  
 	}
       else if(counter<8000001)
 	{
 	  // output8<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output8<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	  output8<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  
 	}
       else if(counter<9000001)
 	{
 	  // output9<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output9<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	  output9<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  
 	}
       else if(counter<10000001)
 	{
 	  // output10<<incident<<" "<<p1<<" "<<i1<<" "<<p2<<" "<<i2<<" "<<p3<<endl;
-	  output10<<p1<<" "<<p2<<" "<<p3<<" "<<i1<<" "<<i2<<" "<<incident<<endl;
+	  output10<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<" "<<p5<<" "<<p6<<" "<<p7<<" "<<p8<<" "<<p9<<" "<<p10<<" "<<i1<<" "<<i2<<" "<<i3<<" "<<i4<<" "<<i5<<" "<<i6<<" "<<i7<<" "<<i8<<" "<<i9<<" "<<incident<<endl;
+	  
 	}
       
       
--- 機械学習/pythonプログラム集/energy_phits3_single.py
+++ 機械学習/pythonプログラム集/energy_phits3_single.py
@@ -11,34 +11,34 @@ import pandas as pd
 import csv #csvファイルの読みこみ
 
 #/home/komori/python_practice/machine_learning_data_set/
-data1 = pd.read_csv('data1_5_1.csv',sep=',')#csvファイルを読み込んだ一番上の行をラベルと認識してくれる 
-data2 = pd.read_csv('data1_5_2.csv',sep=',')#csvファイルを読み込んだ一番上の行をラベルと認識してくれる 
-data3 = pd.read_csv('data1_5_3.csv',sep=',')#csvファイルを読み込んだ一番上の行をラベルと認識してくれる 
+data = pd.read_csv('output1.csv',sep=',')#csvファイルを読み込んだ一番上の行をラベルと認識してくれる 
+#data2 = pd.read_csv('data1_5_2.csv',sep=',')#csvファイルを読み込んだ一番上の行をラベルと認識してくれる 
+#data3 = pd.read_csv('data1_5_3.csv',sep=',')#csvファイルを読み込んだ一番上の行をラベルと認識してくれる 
 
 #print(type(data))
-data1=data1.sample(frac=1)#データのシャッフル
-data1=data1.values#pandasをnumpyに変更
-data2=data2.sample(frac=1)#データのシャッフル
-data2=data2.values#pandasをnumpyに変更
-data3=data3.sample(frac=1)#データのシャッフル
-data3=data3.values#pandasをnumpyに変更
+data=data.sample(frac=1)#データのシャッフル
+data=data.values#pandasをnumpyに変更
+#data2=data2.sample(frac=1)#データのシャッフル
+#data2=data2.values#pandasをnumpyに変更
+#data3=data3.sample(frac=1)#データのシャッフル
+#data3=data3.values#pandasをnumpyに変更
 
-data=np.r_[data1,data2,data3]
+#data=np.r_[data1,data2,data3]
 #print(type(data))
 print(np.shape(data))
 #print(data[4])
 
 #学習用のデータ
-x_train=data[0:2500000,0:10]#配列の0列目からシンチの付与エネルギー情報が入っている
+x_train=data[0:2000000,0:10]#配列の0列目からシンチの付与エネルギー情報が入っている
 #x_train=x_train.round(1)
 #print(np.shape(x_train))
 #print(x_train)
-y_train=data[0:2500000,19]#入射エネルギー情報をとってくる
+y_train=data[0:2000000,19]#入射エネルギー情報をとってくる
 #y_train=y_train.round(1)
 print(y_train)
 #y_train=np.ceil(y_train)
 #y_train=y_train-1
-print(y_train)
+#print(y_train)
 #y_train/=300
 
 #print(np.shape(y_train))
@@ -46,11 +46,11 @@ print(y_train)
 #print(y_train)
 
 #テスト用のデータ
-x_test=data[2500000:2900000,0:10]
+x_test=data[2000000:2500000,0:10]
 #print(np.shape(x_test))
 #print(x_test)
 #x_test=x_test.round(1)
-y_test=data[2500000:2900000,19]
+y_test=data[2000000:2500000,19]
 #y_test=y_test.round(1)
 #print(np.shape(y_test))
 #print(y_test)
@@ -71,15 +71,15 @@ print(np.shape(y_train))
 
 
 model = Sequential()
-model.add(Dense(7,input_dim=10))
+model.add(Dense(3,input_dim=10))
 model.add(Activation('relu'))
 
 
 #model.add(Dense(1000))
 #model.add(Activation('relu'))
 
-model.add(Dense(3))
-model.add(Activation('relu'))
+#model.add(Dense(3))
+#model.add(Activation('relu'))
 
 #model.add(Dense(100))
 #model.add(Activation('relu'))
@@ -118,10 +118,12 @@ val_loss=result.history['val_loss']#検証用データの損失関数
 #print(np.c_[acc,loss,val_acc,val_loss])#配列の結合
 
 path_w3 ='test3.csv'
-
+path_w4 ='correct_output.txt'
 history=np.savetxt(path_w3,np.c_[mae,loss,val_mae,val_loss],delimiter=",",fmt="%s",header='mae,loss,va_mae,val_loss')
 
 predict=model.predict(x_test)
+connect=np.c_[y_test,predict]
+correct_output=np.savetxt(path_w4,connect,delimiter=" ",fmt="%s")
 #path_w1 ='/mnt/test1.csv'
 #path_w2 ='/mnt/c/Users/komori/Desktop/test2.csv'
 
@@ -134,6 +136,8 @@ json_string=model.to_json()
 yaml_string=model.to_yaml()
 model.save_weights('weihgts.h5');
 open('model.json','w').write(json_string)
+
+
 
 
 """
